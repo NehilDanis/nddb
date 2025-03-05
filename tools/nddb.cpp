0528760baa1d7bf3ec1437c754167f509169a938
Nehil Danis <nehild@outlook.com>
2025-03-05 22:32:27 +0100

    feat: created main functionwith arg checks to attach

--- tools/nddb.cpp
+++ tools/nddb.cpp
@@ -1,5 +1,19 @@
 #include <libnddb/libnddb.hpp>
 
-int main() {
-    nddb::say_hello();
+#include <iostream>
+
+namespace {
+    pid_t attach(int argc, const char** argv) {
+
+    }
+}
+
+int main(int argc, const char** argv) {
+    if(argc == 1) {
+        std::cerr << "No arguments given\n";
+        return -1;
+    }
+
+    pid_t pid = attach(argc, argv);
+  
 }
