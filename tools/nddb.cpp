3ff7951e240dce744751f76d369a078b2ebc1e19
Nehil Danis <nehild@outlook.com>
2025-03-05 22:38:11 +0100

    feat: defined two branches in attach to check if pid or program name provided

--- tools/nddb.cpp
+++ tools/nddb.cpp
@@ -1,10 +1,25 @@
 #include <libnddb/libnddb.hpp>
 
+#include <sys/ptrace.h>
+
 #include <iostream>
+#include <string_view>
 
 namespace {
     pid_t attach(int argc, const char** argv) {
+        pid_t pid = 0;
+        // if the user passes pid or a running 
+        // process
+        // nddb -p <pid>
+        if(argc == 3 && argv[1] == std::string_view("-p")) {
 
+        }
+        // if the user provided a program name to launch
+        else {
+
+        }
+
+        return pid;
     }
 }
 
